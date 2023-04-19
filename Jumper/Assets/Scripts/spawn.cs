3c16b28720bcae218f00fa438ff9f59a72a8ea1a
MOMOONS <quinten1.moons@gmail.com>
2023-04-19 12:05:45 +0200

    jumperscript aangepast

--- Jumper/Assets/Scripts/spawn.cs
+++ Jumper/Assets/Scripts/spawn.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
-    public GameObject AgentX;
-    public GameObject AgentZ;
+    public GameObject TargetX;
+    public GameObject TargetZ;
 
     // Use this for initialization
     void Start()
@@ -15,28 +15,26 @@ public class spawn : MonoBehaviour
 
     // Update is called once per frame
     float timer = 10f;
-    bool start = false;
-    public float spawnRateX = 3f;
-    public float spawnRateZ = 3f;
+    bool start = false; 
+    public float spawnRate = 3f;
     void Update()
     {
-        while(true)
-        {
-            if (timer >= spawnRateX)
+            if (timer >= spawnRate)
             {
-                GameObject newAgentX = Instantiate(AgentX, transform.position + transform.forward, AgentX.transform.rotation);
-                newAgentX.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
-                GameObject newAgentZ = Instantiate(AgentZ, transform.position + transform.forward, AgentZ.transform.rotation);
-                newAgentZ.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
-                start = true;
-                timer = 0f;
+               Vector3 positionTargetX = new Vector3(10, (float)0.5, 0);
+               Vector3 positionTargetZ = new Vector3(0, (float)0.5, 20);
+               GameObject newTargetX = Instantiate(TargetX, positionTargetX + transform.forward, TargetX.transform.rotation);
+               newTargetX.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
+               GameObject newTargetZ = Instantiate(TargetZ, positionTargetZ + transform.forward, TargetZ.transform.rotation);
+               newTargetZ.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
+               start = true;
+               timer = 0f;
             }
 
             if (start)
             {
-                timer += Time.deltaTime;
-                start = false;
+               timer += Time.deltaTime;
+               start = false;
             }
-        }
     }
 }
