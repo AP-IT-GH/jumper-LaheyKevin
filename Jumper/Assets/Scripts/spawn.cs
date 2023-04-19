9bbcf9a1d8abc82185a449a49b591f00934bcca1
MOMOONS <quinten1.moons@gmail.com>
2023-04-19 12:14:11 +0200

    spawnlocatie aangepast

--- Jumper/Assets/Scripts/spawn.cs
+++ Jumper/Assets/Scripts/spawn.cs
@@ -21,8 +21,8 @@ public class spawn : MonoBehaviour
     {
             if (timer >= spawnRate)
             {
-               Vector3 positionTargetX = new Vector3(10, (float)0.5, 0);
-               Vector3 positionTargetZ = new Vector3(0, (float)0.5, 20);
+               Vector3 positionTargetX = new Vector3(10, 1, 0);
+               Vector3 positionTargetZ = new Vector3(0, 1, 15);
                GameObject newTargetX = Instantiate(TargetX, positionTargetX + transform.forward, TargetX.transform.rotation);
                newTargetX.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
                GameObject newTargetZ = Instantiate(TargetZ, positionTargetZ + transform.forward, TargetZ.transform.rotation);
