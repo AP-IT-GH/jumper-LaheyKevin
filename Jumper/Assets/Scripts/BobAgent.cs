87a34e633eff8b2283e089b6e28be2dc37545fe3
LaheyKevin <91462775+LaheyKevin@users.noreply.github.com>
2023-04-19 10:59:15 +0200

    update

--- Jumper/Assets/Scripts/BobAgent.cs
+++ Jumper/Assets/Scripts/BobAgent.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BobAgent : MonoBehaviour
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class BobAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform Target;
+    public override void OnEpisodeBegin()
     {
-        
+        // reset de positie en orientatie als de agent gevallen is
+        if (transform.localPosition.z != 0)
+        {
+            transform.localPosition = new Vector3(0, 0.25f, 0);
+            transform.localRotation = Quaternion.identity;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        // Target enAgent posities
+        //sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+    }
+
+    public float speedMultiplier = 0.1f;
+    public float rotationMultiplier = 5;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Acties, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0]; ;
+        transform.Translate(controlSignal * speedMultiplier);
+        transform.Rotate(0.0f, rotationMultiplier * actionBuffers.ContinuousActions[1], 0.0f);
+
+        // Beloningen
+        float distanceToTarget = Vector3.Distance(transform.localPosition, Target.localPosition);
+
+        // target bereikt
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        // Van hetplatform gevallen?
+        else if (transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            continuousActionsOut[0] = 1;
+        }
     }
 }
